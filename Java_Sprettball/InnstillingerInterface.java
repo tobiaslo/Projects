827acc8a6fe04f05a3ba906a8e3106c3712aaacc
Tobias Lømo <tobiaslo@uio.no>
2020-09-26 20:15:58 +0200

    delete sprettball

--- Java_Sprettball/InnstillingerInterface.java
+++
@@ -1,8 +0,0 @@
-import javafx.scene.layout.VBox;
-
-public interface InnstillingerInterface {
-    public VBox hentLayout();
-    public String hentNavn();
-    public void save();
-    public void startDen();
-}
